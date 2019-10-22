a902a6fc112f1291163dbf84b7efe1226eeee388
Wolfgang Figl <wolfgang.figl@me.com>
2019-10-22 10:41:17 +0200

    basic2

--- basic/js/xmas-script.js
+++ basic/js/xmas-script.js
@@ -11,3 +11,12 @@ $(".xmasImg").click(function(){
 /* $(".xmasImg").on("click", function(){
  	showMsg($(this).attr("id"));
 }); */
+
+
+
+$(".xmasImg2").click(function(){
+	let x = $(this).attr("id");
+	$("#"+x).remove();
+});
+
+// sobald du xmasimg2 anklickst, schaut er welche ID der santa hat, und löscht den jenigen santa.
